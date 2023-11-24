d8abcca8e17e8789d8fcd5bf7c2c64e6360ea3e4
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2023-11-24 10:03:06 +0100

    commit du job13

--- jour04/job13/main.py
+++ jour04/job13/main.py
@@ -1,16 +1,12 @@
-def pas_de_doublons(liste):
-    liste_sans_gemeaux = []
-    for element in liste:
-        if not any(element == x for x in liste_sans_gemeaux):
-            liste_sans_gemeaux = liste_sans_gemeaux + [element]
-    return liste_sans_gemeaux
+L_initial = [10, 20, 30, 20, 10, 50, 60, 40, 80, 50, 40]
+L_tempo = []
+print(L_initial)
 
-# Liste initiale
-liste_initiale = [10, 20, 30, 20, 10, 50, 60, 40, 80, 50, 40]
+for i in L_initial:
+    if i not in L_tempo:
+        L_tempo = L_tempo + [i] 
 
-# Supprimer les doublons
-liste_sans_gemeaux = pas_de_doublons(liste_initiale)
 
-# Afficher la liste sans doublons
-print(liste_initiale)
-print(liste_sans_gemeaux)
+print(L_tempo)
+
+        
